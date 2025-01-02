ba4691a4d076a1c13a25041fcc9af7527d2aa9e5
Your Name <liyanafaisel@gmail.com>
2025-01-02 16:38:49 +0100

    SignUp Form- Form Validation

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { 
+  AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon'
-import { RouterModule } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
+import {MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-signin',
@@ -18,7 +19,8 @@ import { RouterModule } from '@angular/router';
     MatButtonModule, 
     MatIconModule, 
     ReactiveFormsModule, 
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss'],
@@ -26,18 +28,68 @@ import { RouterModule } from '@angular/router';
 
 export class SigninComponent implements OnInit {
 
-  form!: FormGroup;
-  hide = true;
-  
-  constructor( 
-    private formBuilder: FormBuilder
-  ) {}
+  signInForm!: FormGroup;
+  signUpForm!: FormGroup;
 
-  ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      email: ['',[Validators.required, Validators.email]],
-      password: ['',[Validators.required]]
+  durationInSeconds = 2;
+
+  isSignUpClicked: boolean = false;
+  isFormHidden: boolean = false;
+  hide = true;
+  hideRepeatPassword = true;
+
+  private _snackBar = inject(MatSnackBar);
+
+  constructor(private formBuilder: FormBuilder) 
+  {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+
+    this.signUpForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required],
     });
   }
+
+  ngOnInit(): void {
+  }
+
+  checkPasswords() {
+    const password = this.signUpForm.get('password')?.value;
+    const repeatPassword = this.signUpForm.get('repeatPassword')?.value;
+    if (password !== repeatPassword) {
+      this.signUpForm.get('repeatPassword')?.setErrors({ passwordMismatch: true });
+      this.signUpForm.get('repeatPassword')?.markAsTouched(); 
+    } else {
+      this.signUpForm.get('repeatPassword')?.updateValueAndValidity();
+    }
+  }
+  
+  onToRegister() {
+    this.isFormHidden = true;
+    this.isSignUpClicked = true;
+    this.signInForm.reset();
+  }
+
+  onToLogin() {
+    this.isFormHidden = true;
+    this.isSignUpClicked = false;
+    this.signUpForm.reset();
+  }
+
+  onRegisterConfirm(password: string, repeatPassword: string, config?: MatSnackBarConfig) {
+    if(password === repeatPassword){
+      const snackBarRef = this._snackBar.open("Successfully registered", "Okay");
+    
+      // After the snackbar message is clicked Okay, set isSignUpClicked to false to show the Signin Page, after succesfull Registeration
+      snackBarRef.onAction().subscribe(() => {
+        this.isSignUpClicked = false;
+      });
+    }
+  }
+  
 
 }
